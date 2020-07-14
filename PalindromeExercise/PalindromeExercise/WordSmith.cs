83f137f54fc4e540d0c5c990d349396ea2e7117f
Grayson Owen <graysonowen@Graysons-MacBook-Pro.local>
2020-07-14 14:38:32 -0500

    racecarBackwards

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        
+        public bool IsAPalindrone(string word)
+        {
+            var reversed = "";
+
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                reversed += word[i];
+            }
+
+
+            if(reversed.ToLower() == word.ToLower())
+            {
+                return true;    
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        
+
+
+
+
+
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,35 @@
 using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("whatsup", false)]
+        [InlineData("kayak", true)]
+        [InlineData("Madam", true)]
+        [InlineData("kitteN", false)]
+        public void Test1(string word, bool expected)
         {
 
+            var tester = new WordSmith();
+
+            var actual = tester.IsAPalindrone(word);
+
+            Assert.Equal(expected, actual);
         }
+
+
+
+
+
+
+
+
+
+
     }
 }
